cc5791ba5869d00964ad8c2df79874550697ef2e
Yaroslav <gamerplay0606@gmail.com>
2024-09-07 19:05:48 +0300

    update

--- src/main/java/com/yaryy/SpringBootREST/JobRESTController.java
+++ src/main/java/com/yaryy/SpringBootREST/JobRESTController.java
@@ -3,15 +3,12 @@ package com.yaryy.SpringBootREST;
 import com.yaryy.SpringBootREST.model.JobPost;
 import com.yaryy.SpringBootREST.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
-@RestController
+@RestController // to return a data not a view
 public class JobRESTController {
 
     @Autowired
@@ -25,6 +22,10 @@ public class JobRESTController {
     @GetMapping("jobPost/{postId}")
     public JobPost getJob(@PathVariable("postId") int postId){
         return service.getJob(postId);
+    }
+    @PostMapping("jobPost")
+    public void addJob(@RequestBody JobPost jobPost){ // to return json data
+        service.addJob(jobPost);
     }
 
 }
