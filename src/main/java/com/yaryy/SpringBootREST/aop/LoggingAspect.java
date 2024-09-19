1d829e5cb6c234fe336d8b9b47fd736a076365df
Yaroslav <gamerplay0606@gmail.com>
2024-09-19 14:09:01 +0300

    update

--- src/main/java/com/yaryy/SpringBootREST/aop/LoggingAspect.java
+++ src/main/java/com/yaryy/SpringBootREST/aop/LoggingAspect.java
@@ -1,7 +1,7 @@
 package com.yaryy.SpringBootREST.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,9 +18,24 @@ public class LoggingAspect {
     // args
 
                     // return type, class-name.method-name (args)
-    @Before("execution(* com.yaryy.SpringBootREST.service.JobService.*(..))")
-    public void logMethodCall(){
-        LOGGER.info("Method called");
+    @Before("execution(* com.yaryy.SpringBootREST.service.JobService.getAllJobs(..)) || execution(* com.yaryy.SpringBootREST.service.JobService.getJob(..))")
+    public void logMethodCall(JoinPoint joinPoint){
+        LOGGER.info("Method called" + " " +joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* com.yaryy.SpringBootREST.service.JobService.getAllJobs(..)) || execution(* com.yaryy.SpringBootREST.service.JobService.getJob(..))")
+    public void logMethodExecuted(JoinPoint joinPoint){
+        LOGGER.info("Method Executed" + " " +joinPoint.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.yaryy.SpringBootREST.service.JobService.getAllJobs(..)) || execution(* com.yaryy.SpringBootREST.service.JobService.getJob(..))")
+    public void logMethodException(JoinPoint joinPoint){
+        LOGGER.info("Method has some issues" + " " +joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.yaryy.SpringBootREST.service.JobService.getAllJobs(..)) || execution(* com.yaryy.SpringBootREST.service.JobService.getJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint joinPoint){
+        LOGGER.info("Method Executed Successfully" + " " +joinPoint.getSignature().getName());
     }
 
 }
